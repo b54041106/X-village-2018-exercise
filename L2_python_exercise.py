142a850cde1bac1703380f0c35d5e8e2da7bb053
b54041106 <40970136+b54041106@users.noreply.github.com>
2018-07-12 15:28:26 +0800

    add prime number and print star vy the while loop

--- L2_python_exercise.py
+++ L2_python_exercise.py
@@ -1,140 +1,95 @@
-def maker1(n):   #n=4-->maker1(4)=b
-    def action(x): #x=2 
-        return x**n 
-    return action
-
-b= maker1(4) 
-#print(b(2))
-
-
-##印1~100奇數
-a = 1
-while a < 100:
-    #print(a, end = ' ')
-    a += 2
-#print("end")
-##比較
-#a = 1
-#while a < 100:    #loop 
-#    print(a, end = ' ')
-#print("end")= 1 1 1 1 1 1 1 ... 
-
-#n = 100
-#iterate = 0
-#while iterate != n:
-    #iterate += 1
-    #print(iterate, end = ' ')
-
-#n = 100
-#iterate = 0
-#total = 0
-#while iterate != n:
-#    iterate += 1    #inteate+1
-#    total += iterate #interate+interate
-#print(total)
-
-
-#a = 5
-#while a < 7:
-#    if (a % 2 == 0):
-#        print(a, "is even")
-#    else:
-#        print(a, "is odd")
-#    a += 1
-##frontward
-#n = 0
-#ans = 0
-#while n <= 1000:
-#    if (n % 11 == 0):
-#        ans = n
-#    n += 1
-#print(ans)
-##backward+break停止
-#n = 1000
-#flag = True
-#while n > 0:
-#    if (n % 11 == 0) and flag:
-#        print("correct", n)
-#        flag = False
-#        break
-#    print(n)
-#    n -= 1
-
-# def number(n):
+#Exercise>>終極密碼
+#def number(n):
 import random
 number=random.randint(1,100)  #電腦隨機選一個幸運號碼
-#print(number)
+print(number)
 guess_number=23
 while guess_number!=number:
     if number<guess_number:  
-        #print(guess_number)
-        #print ("too small")
+        print(guess_number)
+        print ("too small")
         guess_number-=1
     elif number>guess_number: 
-        #print(guess_number)
-        #print ("too large")
+        print(guess_number)
+        print ("too large")
         guess_number+=1
-    #print("Correct")
+    print("Correct")
 
+#Exercise>>9*9乘法表
 i = 1   # 被乘數
 j = 1   # 乘數
 while i <= 9:
     while j <= 9:
-        #print(i, '*', j, '=', i* j, sep='', end='\t')
+        print(i, '*', j, '=', i* j, sep='', end='\t')
         j += 1
-    #print("")
+    print("")
     j = 1
     i += 1
 
-n = 9
-i = 1
-j = 1
-while i <= n:
-    while j <= 5:
-    #    print(i, '*', j, '=', i* j, sep='', end='\t')
-        j += 1
-   # print("")
-    j = 1
-    i += 1
-
-n = 9
-i = 1
-j = 1
-while i <= n:
+#Exercis>>nested while loop_Method1
+n = 5
+i = 1   # 被乘數
+while i <= 5:
+    j= 1
     while j <= n:
-    #    print(i, j)
+        print('*',sep='',end='\t')
         j += 1
-    #print("")
-    j = 1
-    i += 1
+    print('\n')
+    n -=1
+    i +=1
+#Exercise>>nested while loop_Method2_square
+n=5
+a=99999
+while a>0:
+    a=n
+    b=n
+    while b>0:
+        print("*",sep="\t",end=" ")
+        b-=1
+    print("\n")
+    n-=1
 
-#i = 1
-#j = 1
-#while i <= 9:
-#    while j <= 9:
-#        if i*j < 50:
-#           print(i, '*', j, '=', i* j, sep='', end='\t')
-#        j += 1
-#    print("")
-#    j = 1
-#    i += 1
+n=5
+a=0
+while a<n:
+    b=0
+    while(b<n):
+        if b<=(a-1):
+            print(" ",end="")
+        else:
+            print("*",end="")
+        b+=1
+    print("\n")
+    a+=1
 
-i = 1
-j = 1
-while i <= 9:
-    while j <= 9:
-        if i*j > 50:
-            break
-        #print(i, '*', j, '=', i* j, sep='', end='\t')
-        j += 1
-    #print('')
-    j = 1
-    i += 1
+for i in range(1, 5):
+     for j in range(1, 5):
+         if j>=i:
+            print("*"),
+     print("\n")
 
-while i <= n:
-    while j <= 5:
-    #    print(i, '*', j, '=', i* j, sep='', end='\t')
-        j += 1
-   # print("")
-    j = 1
-    i += 1
+for i in range(1,7):
+    for j in range(6-i):
+        print(" ",end="")
+    for j in range(1,1*i):
+        print("*",end="")
+    print("\n")
+
+for i in range(1,7):
+    for j in range(i-6):
+        print(" ",end="")
+    for j in range(1,1*i):
+        print("*",end="")
+    print("\n")
+
+##Exercise>> for loop  50以內的質數  
+num=[]
+i=2
+for i in range(2,50):
+   j=2
+   for j in range(2,i):
+      if(i%j==0):
+         break
+   else:
+      num.append(i)
+print(num)
